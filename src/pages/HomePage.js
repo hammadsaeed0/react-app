8931b2014f6998402b7e2407730b3c2a141e6492
zhaider113 <zhaider113@gmail.com>
2023-08-30 04:19:59 +0500

    client dashboard, post job, client profile find talent, proposal page client sidenot found, server page done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,9 +5,21 @@ import { Routes } from "../routes";
 import FrontPage from "./FrontPage";
 import Signup from "./auth/Signup";
 import Signin from "./auth/Signin";
+import ClientDashboard from "./client_dashboard/Dashboard";
+import ClientProposal from "./client_dashboard/Proposal";
+import PostJob from "./client_dashboard/PostJob";
+import ClientProfile from "./client_dashboard/Profile";
+import TalentSearch from "./client_dashboard/FindTalent";
+
+// extra pages 
+
+import NotFoundPage from "./NotFound";
+import ServerError from "./ServerError";
 
 // components
 import Preloader from "../components/Preloader";
+import TopNavbar from '../components/TopNavbar';
+import Footer from "../components/Footer";
 
 const RouteWithLoader = ({ component: Component, ...rest }) => {
     const [loaded, setLoaded] = useState(false);
@@ -22,12 +34,58 @@ const RouteWithLoader = ({ component: Component, ...rest }) => {
     );
   };
 
+  const RouteWithSidebarClient = ({ component: Component, ...rest }) => {
+    const [loaded, setLoaded] = useState(false);
   
+    useEffect(() => {
+      const timer = setTimeout(() => setLoaded(true), 1000);
+      return () => clearTimeout(timer);
+    }, []);
+  
+    const localStorageIsSettingsVisible = () => {
+      return localStorage.getItem('settingsVisible') === 'false' ? false : true
+    }
+  
+    const [showSettings, setShowSettings] = useState(localStorageIsSettingsVisible);
+  
+    const toggleSettings = () => {
+      setShowSettings(!showSettings);
+      localStorage.setItem('settingsVisible', !showSettings);
+    }
+  
+    return (
+      <Route {...rest} render={props => (
+        <>
+          <Preloader show={loaded ? false : true} />
+          {/* <Sidebar /> */}
+  
+            <TopNavbar />
+          <main className="content">
+            {/* <Navbar /> */}
+            <Component {...props} />
+            <Footer toggleSettings={toggleSettings} showSettings={showSettings} />
+          </main>
+        </>
+      )}
+      />
+    );
+  };
+
+
 const HomePage = () => (
     <Switch>
       <RouteWithLoader exact path={Routes.FrontPage.path} component={FrontPage} />
       <RouteWithLoader exact path={Routes.Signin.path} component={Signin} />
       <RouteWithLoader exact path={Routes.Signup.path} component={Signup} />
+      <RouteWithLoader exact path={Routes.NotFound.path} component={NotFoundPage} />
+      <RouteWithLoader exact path={Routes.ServerError.path} component={ServerError} />
+
+      <RouteWithSidebarClient exact path={Routes.ClientDashboard.path} component={ClientDashboard} />
+      <RouteWithSidebarClient exact path={Routes.ClientProposal.path} component={ClientProposal} />
+      <RouteWithSidebarClient exact path={Routes.PostJob.path} component={PostJob} />
+      <RouteWithSidebarClient exact path={Routes.ClientProfile.path} component={ClientProfile} />
+      <RouteWithSidebarClient exact path={Routes.TalentSearch.path} component={TalentSearch} />
+      
     </Switch>
 );
 export default HomePage;
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,13 @@ export const Routes = {
     FrontPage: { path: "/" },
     Signup: { path: "/sign-up" },
     Signin: { path: "/sign-in" },
+    SubmitProposal: { path: "/submit-proposal" },
+    ClientDashboard: { path: "/client-dashboard" },
+    ClientProposal: { path: "/client-proposal" },
+    PostJob: { path: "/post-job" },
+    ClientProfile: { path: "/client-profile" },
+    TalentSearch: { path: "/talent" },
+
+    NotFound: { path: "/examples/404" },
+    ServerError: { path: "/examples/500" },
 };
