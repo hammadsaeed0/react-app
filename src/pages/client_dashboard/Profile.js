6a6a1ce7207c78ef94dcb66eba226f9f148314ec
zhaider113 <zhaider113@gmail.com>
2023-09-07 23:45:58 +0500

    chat screen updated

--- src/pages/client_dashboard/Profile.js
+++ src/pages/client_dashboard/Profile.js
@@ -585,7 +585,7 @@ const Profile = () => {
                         </Card>
                     </Col>
                     {/* work History */}
-                    <Col xs={12} xl={12} md={11} className="mb-2 mt-3 p-4">
+                    {/* <Col xs={12} xl={12} md={11} className="mb-2 mt-3 p-4">
                         <h4 className="mb-0 project-count-heading heading20">Work History and Feedback</h4>
                         <hr className="red-line  border-bottom"/>
                     <Tab.Container defaultActiveKey="completed">
@@ -640,7 +640,7 @@ const Profile = () => {
                         </Col>
                         </Row>
                     </Tab.Container>
-                    </Col>
+                    </Col> */}
                 </Row>
             </Col>
 
@@ -657,11 +657,7 @@ const Profile = () => {
                       </Col>
                       <Col xs={12} xl={12} md={12} className="mt-5">
                         <h5 className="project-count-heading font-encode">Verification</h5>
-                        <p className="fund-subheading font-600 line-height-1">Phone Number: <span className="text-dark-gry ">Verified</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
-                            <path d="M11.6071 0H1.39286C0.623594 0 0 0.623594 0 1.39286V11.6071C0 12.3764 0.623594 13 1.39286 13H11.6071C12.3764 13 13 12.3764 13 11.6071V1.39286C13 0.623594 12.3764 0 11.6071 0ZM11.6071 11.6071H1.39286V1.39286H11.6071V11.6071ZM10.5664 4.59283L5.55828 9.5608C5.42175 9.69623 5.20128 9.69536 5.06585 9.55883L2.43158 6.90323C2.29615 6.7667 2.29702 6.54623 2.43358 6.41077L3.09284 5.75682C3.22937 5.6214 3.44985 5.62227 3.58527 5.75883L5.32031 7.50793L9.42204 3.43917C9.55857 3.30374 9.77905 3.30461 9.91447 3.44114L10.5684 4.10037C10.7038 4.23693 10.703 4.4574 10.5664 4.59283Z" fill="#1680FB"/>
-                            </svg>
-                        </p>
+                        <p className="fund-subheading font-600 line-height-1">Phone Number: </p>
                         <p className="fund-subheading font-600 line-height-1">Email: <span className="text-dark-gry">Verified</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
                             <path d="M11.6071 0H1.39286C0.623594 0 0 0.623594 0 1.39286V11.6071C0 12.3764 0.623594 13 1.39286 13H11.6071C12.3764 13 13 12.3764 13 11.6071V1.39286C13 0.623594 12.3764 0 11.6071 0ZM11.6071 11.6071H1.39286V1.39286H11.6071V11.6071ZM10.5664 4.59283L5.55828 9.5608C5.42175 9.69623 5.20128 9.69536 5.06585 9.55883L2.43158 6.90323C2.29615 6.7667 2.29702 6.54623 2.43358 6.41077L3.09284 5.75682C3.22937 5.6214 3.44985 5.62227 3.58527 5.75883L5.32031 7.50793L9.42204 3.43917C9.55857 3.30374 9.77905 3.30461 9.91447 3.44114L10.5684 4.10037C10.7038 4.23693 10.703 4.4574 10.5664 4.59283Z" fill="#1680FB"/>
